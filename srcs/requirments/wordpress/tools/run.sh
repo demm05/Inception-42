fd2db4b2e4fb0b717c0ba7b9ec8ee03190b4f2f1
Dmytro Melnyk <179873458+demm05@users.noreply.github.com>
2025-10-24 18:28:03 +0100

    initial setup of wordpress is done but having problem with php-fpm no suck packet

--- srcs/requirments/wordpress/tools/run.sh
+++ srcs/requirments/wordpress/tools/run.sh
@@ -1,18 +1,53 @@
+#!/bin/bash
+
+set -e
+UPSTREAM_URL=https://wordpress.org/wordpress-6.8.3.tar.gz
+
 if [ -n "$DB_PASSWORD_FILE" ]; then
 	export DB_PASSWORD=$(cat "$DB_PASSWORD_FILE")
 fi
 
-if [ -z "$DB_DATABASE" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ]; then
+if [ -z "$DB_NAME" ] || [ -z "$DB_USER" ] || [ -z "$DB_PASSWORD" ] || [ -z "$DB_HOST" ]; then
 	echo "[!] ERROR: One or more required environment variables are not set."
 	exit 1
 fi
 
-cat << EOF > /etc/wordpress/config-$DOMAIN_NAME.php
+if [ -d "/srv/www/wordpress/wp-config.php" ]; then
+	echo "[i] Wordpress is already installed."
+else
+    mkdir -p /srv/www
+    chown www-data: /srv/www
+
+    curl -fsSL $UPSTREAM_URL | tar zx -C /srv/www
+    chown www-data: /srv/www/wordpress
+
+    SALTS=$(curl -fsSL https://api.wordpress.org/secret-key/1.1/salt/)
+
+    cat << EOF > /srv/www/wordpress/wp-config.php 
 <?php
 define('DB_NAME', '$DB_NAME');
 define('DB_USER', '$DB_USER');
 define('DB_PASSWORD', '$DB_PASSWORD');
-define('DB_HOST', 'localhost');
+define('DB_HOST', '$DB_HOST');
 define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
+
+$SALTS
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', false );
+
+if ( ! defined( 'ABSPATH' ) ) {
+        define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
+
 ?>
 EOF
+
+    chown www-data: /srv/www/wordpress/wp-config.php
+
+fi
+
+php8.2-fpm -F
